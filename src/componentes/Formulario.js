4f016d2259222b04802cc780231dfa3e25042fe5
gescobar-technisys <gescobar@technisys.com>
2018-10-02 11:45:19 -0300

    Creo formulario de consulta de clima por ciudad y pais

---
+++ src/componentes/Formulario.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Formulario extends Component {
+    render() {
+        return (
+            <div className="contenedor-form">
+                <div className="container">
+                    <div className="row">
+                        <form>
+                            <div className="input-field col s12 m8 l4 offset-m2">
+                                <input id="ciudad" type="text" />
+                                <label htmlFor="ciudad">Ciudad: </label>
+                            </div>
+                            <div className="input-field col s12 m8 l4 offset-m2">
+                                <select>
+                                    <option value ="" defaultValue>Elige un país</option>
+                                    <option value="AR">Argentina</option>
+                                    <option value="CO">Colombia</option>
+                                    <option value="CR">Costa Rica</option>
+                                    <option value="ES">España</option>
+                                    <option value="US">Estados Unidos</option>
+                                    <option value="MX">México</option>
+                                    <option value="PE">Perú</option>
+                                    <option value="BR">Brasil</option>
+                                </select>
+                                <label htmlFor="pais">Pais: </label>
+                            </div>
+                            <div className="input-field col s12 m8 l4 offset-2 buscador">
+                                <input type="submit" className="waves-effect waves-light btn-large yellow accent-4" value="Buscar..." />
+                                 
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Formulario;
