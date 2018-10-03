9922e19deaaaf0931c3e603b4d995f695084e719
gescobar-technisys <gescobar@technisys.com>
2018-10-03 15:25:28 -0300

    Agrego icono del clima segun la ciudad seleccionada

--- src/componentes/Clima.js
+++ src/componentes/Clima.js
@@ -10,16 +10,16 @@ class Clima extends Component {
 
         const kelvin = 273.15;
         const urlIcono = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
-
+        const alt="`clima de ${name}`";
         return(
             <div className="row">
                 <div className="resultado col s12 m8 l6 offset-m2 offset-l3">
                     <div className="card-panel light-blue align-center">
                         <span className="white-text">
-                            <h2>Resultado Clima de: {name}</h2>
+                            <h2>Clima de: {name}</h2>
                             <p className="temperatura">
                                 Actual:{(main.temp - kelvin).toFixed(2)} &deg;C
-                                <img src={urlIcono} alt="`clima de ${name}`" />
+                                <img src={urlIcono} alt={alt} />
                             </p>
                             <p>Max. {main.temp_max - kelvin} &deg;C</p>
                             <p>Min. {main.temp_min - kelvin} &deg;C</p>
